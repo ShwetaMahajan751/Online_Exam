56dc0e8a5b0d14af9e466c289382b860c82668a1
Shweta Mahajan <98331561+ShwetaMahajan751@users.noreply.github.com>
2023-03-29 18:31:02 +0530

    Create ES_OTP_LOG.cs

---
+++ Models/ES_OTP_LOG.cs
@@ -0,0 +1,39 @@
+namespace OnlineExam.Models
+{
+    public class Otp_Log
+    {
+        public long Log_Id { get; set; }
+
+        public byte Intimation_Type_Enum { get; set; }
+
+        public string User_Name { get; set; }
+
+        public string? OTP { get; set; }
+
+        public bool? Is_Valid { get; set; }
+
+        public bool? Is_Verified { get; set; }
+
+        public byte? Login_Through_Enum { get; set; }
+
+        public DateTime? Verified_Datetime { get; set; }
+
+        public long? Login_User_Id { get; set; }
+
+        public bool? Is_Requested_Number_Change { get; set; }
+
+        public long? Person_Id { get; set; }
+
+        public bool? Status_Enum { get; set; }
+
+        public long? Created_By { get; set; }
+
+        public DateTime? Created_Datetime { get; set; }
+
+        public long? Modified_By { get; set; }
+
+        public DateTime? Modified_Datetime { get; set; }
+
+        public int? Lock_Id { get; set; }
+    }
+}
