17b9c78076f2638425fc73cb58d93fcc667d9016
Shweta Mahajan <98331561+ShwetaMahajan751@users.noreply.github.com>
2023-03-29 18:41:32 +0530

    Update VAL_User.cs

--- ValidationLayer/VAL_User.cs
+++ ValidationLayer/VAL_User.cs
@@ -1,1 +1,24 @@
+namespace OnlineExam.ValidationLayer
+{
+    public class Val_User1
+    {
+        public bool IsAnyError { get; internal set; }
+        public Dictionary<string, string> ErrorMessages { get; internal set; }
+
+        internal void GetEnumerators()
+        {
+
+        }
+
+        internal void User_Login()
+        {
+
+        }
+
+        internal void Verify_Login()
+        {
+
+        }
+    }
+}
 
